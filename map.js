f38245de754cc2a94c7600c197b05fb619d66f0b
benni maxon <benni.maxon@gmail.com>
2023-01-18 10:04:40 -0800

    all .map tests passing

--- map.js
+++ map.js
@@ -2,19 +2,16 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {
-  const resp = countries.map((item) => item.name);
-  return resp;
-};
+export const getCountryNames = (countries) => countries.map((item) => item.name);
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => countries.map((item) => [item.name, item.iso2]);
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
 // REQS: use .map
 
-export const getBrands = (brands) => {};
+export const getBrands = (brands) => brands.map((item) => item.brand);
