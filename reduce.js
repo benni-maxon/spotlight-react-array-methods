764fc429bfe5467cf25b72735d3773d58abff09a
benni maxon <benni.maxon@gmail.com>
2023-01-18 11:16:44 -0800

    all tests passing

--- reduce.js
+++ reduce.js
@@ -7,4 +7,7 @@ export const getSneaks = (brands) => brands.reduce((acc, brand) => [...acc, ...b
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const ageSum = dogs.reduce((acc, dog) => (acc += dog.age), 0);
+  return ageSum / dogs.length;
+};
