3ad0240b2108897dff455f7e2b181052960ac705
benni maxon <benni.maxon@gmail.com>
2023-01-18 12:26:29 -0800

    refactored dog.age

--- reduce.js
+++ reduce.js
@@ -8,6 +8,6 @@ export const getSneaks = (brands) => brands.reduce((acc, { shoes }) => [...acc, 
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
 export const getAverageAge = (dogs) => {
-  const ageSum = dogs.reduce((acc, dog) => (acc += dog.age), 0);
+  const ageSum = dogs.reduce((acc, { age }) => (acc += age), 0);
   return ageSum / dogs.length;
 };
